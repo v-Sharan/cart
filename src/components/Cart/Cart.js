5ca5a45b99ac68762b721dab41e3421be11db66d
v-Sharan <vsharansago@gmail.com>
2023-07-12 11:32:25 +0530

    changes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,7 +7,7 @@ const Cart = (props) => {
   const cartItems = useSelector((state) => state.cart.items);
   return (
     <Card className={classes.cart}>
-      <h2>Your Shopping Cart</h2>
+      <h2>Your Cart</h2>
       <ul>
         {cartItems.map((item) => (
           <CartItem
